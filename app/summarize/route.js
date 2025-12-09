4a4207939a5748a1431588764e3ea98dca945720
silunaya <89127585+silunaya@users.noreply.github.com>
2025-12-10 02:47:17 +0530

    Delete app/summarize directory

--- app/summarize/route.js
+++
@@ -1,7 +0,0 @@
-import { callGroq } from "../_shared";
-
-export async function POST(req) {
-  const { text } = await req.json();
-  const output = await callGroq(`Summarize this text:\n\n${text}`);
-  return new Response(JSON.stringify({ output }), { status: 200 });
-}
