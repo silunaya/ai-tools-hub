8d7142f4b23380429744040279507c3243a286cb
silunaya <89127585+silunaya@users.noreply.github.com>
2025-12-10 02:55:49 +0530

    Create route.js

---
+++ app/api/meta/route.js
@@ -0,0 +1,7 @@
+import { callGroq } from "../_shared";
+
+export async function POST(req) {
+  const { text } = await req.json();
+  const output = await callGroq(`Generate SEO meta description for:\n\n${text}`);
+  return new Response(JSON.stringify({ output }), { status: 200 });
+}
