0e2042e12681b600163dd3d93b75dd9861822c23
silunaya <89127585+silunaya@users.noreply.github.com>
2025-12-11 02:07:35 +0530

    Delete app/components/Footer.jsx

--- app/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-<html lang="en">
-  <body className="bg-gray-50 text-gray-900 min-h-screen flex">
-    <Navbar />
-    <main className="ml-64 w-full p-4">
-      {children}
-    </main>
-  </body>
-</html>
-
